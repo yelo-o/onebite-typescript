b758c8e438aa758c87ea4c09e90b555c94d65af5
yelo-o <flyordig@gmail.com>
2025-10-04 11:19:14 +0900

    Add: 타입 추론 예제 추가

---
+++ section3/src/chapter5.ts
@@ -0,0 +1,39 @@
+/**
+ * 타입 추론
+ */
+
+let a = 10;
+let b = "hello";
+let c = {
+    id: 1,
+    name: "이아무개",
+    profile: {
+        nickname: "yelosta",
+    },
+    urls: ["https://yelosta.com"],
+};
+
+let { id, name, profile } = c;
+
+let [one, two, three] = [1, "hello", true];
+
+function func(message = "hello") {
+    return "hello";
+}
+
+/**
+ * any 타입의 진화 (초기값이 없는 경우)
+ */
+let d;
+d = 10; // 타입이 number로 추론 됨
+d.toFixed();
+
+d = "hello";
+d.toUpperCase(); // 타입이 string으로 추론 됨
+// d.toFixed();
+
+// const 변수는 리터럴 타입으로 추론 됨
+const num = 10;
+const str = "hello";
+
+let arr = [1, "string"];
