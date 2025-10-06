6ef6e86dc04ff848640bb0b1248702c8b07e2813
yelo-o <flyordig@gmail.com>
2025-10-06 10:37:48 +0900

    Add: 함수 타입 표현식과 호출 시그니처

---
+++ section4/dist/chapter1.js
@@ -0,0 +1,10 @@
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const add2 = (a, b) => a + b;
+const sub2 = (a, b) => a - b;
+const multiply2 = (a, b) => a * b;
+const divide2 = (a, b) => a / b;
+add2.name = ""; // 객체처럼 활용도 가능
+export {};
