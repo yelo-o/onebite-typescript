77d8be16e8978c9ff8d1c4345ecc70e09d8011fa
yelo-o <flyordig@gmail.com>
2025-10-10 09:52:10 +0900

    Add: map, forEach 메서드 타입 정의하기 예제 추가

---
+++ section7/dist/chapter2.js
@@ -0,0 +1,27 @@
+/**
+ * map 메서드
+ */
+const arr = [1, 2, 3];
+const newArr = arr.map((it) => it * 2);
+function map(arr, callback) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(callback(arr[i]));
+    }
+    return result;
+}
+map(arr, (it) => it * 2);
+map(["hi", "hello"], (it) => parseInt(it));
+/**
+ * forEach 메서드
+ */
+const arr2 = [1, 2, 3];
+arr2.forEach((it) => console.log(it));
+function forEach(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i]);
+    }
+}
+forEach(arr2, (it) => console.log(it.toFixed()));
+forEach(["123", "456"], (it) => console.log(it.toUpperCase()));
+export {};
