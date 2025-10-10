f67f6d855c33e33f736dcee1e7c4504360c83a81
yelo-o <flyordig@gmail.com>
2025-10-10 10:40:25 +0900

    Add: 프로미스와 제네릭 예제 추가

---
+++ section7/dist/chapter5.js
@@ -0,0 +1,33 @@
+/**
+ * 프로미스
+ */
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve(20);
+        reject("~ 때문에 실패");
+    }, 3000);
+});
+promise.then((response) => {
+    console.log(response * 10); // response = 20
+});
+promise.catch((err) => {
+    if (typeof err === "string") {
+        console.log(err);
+    }
+});
+function fetchPost() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                id: 1,
+                title: "게시글 제목1",
+                content: "게시글 컨텐츠1",
+            });
+        }, 3000);
+    });
+}
+const postRequest = fetchPost();
+postRequest.then((post) => {
+    console.log(post.id);
+});
+export {};
